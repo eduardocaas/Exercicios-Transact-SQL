a208f3d530bec5f9408075fb4b677c7e0f2fde33
Eduardo Castro <104233554+eduardocaas@users.noreply.github.com>
2023-05-01 23:11:30 -0300

    Exercício 8 - Procedure

---
+++ Exercicio_8_Procedure.sql
@@ -0,0 +1,29 @@
+/*-------CONSULTA PARA IDENTIFICAR DADOS-------*/
+SELECT vc.*, sp.Bonus FROM VW_COLABORADORES vc INNER JOIN Sales.SalesPerson sp ON vc.BusinessEntityID = sp.BusinessEntityID;
+
+/*-------PROCEDURE RETORNA BÔNUS DE COLABORADOR-------*/
+CREATE PROCEDURE usp_RetornaBonus
+	@idColaborador NUMERIC,
+	@nomeColaborador VARCHAR(200),
+	@totalBonus MONEY OUT
+AS 
+BEGIN 
+	SELECT @totalBonus = sp.Bonus
+	FROM VW_COLABORADORES vc 
+	INNER JOIN Sales.SalesPerson sp
+	ON vc.BusinessEntityID = sp.BusinessEntityID
+	WHERE vc.BusinessEntityID = @idColaborador
+	OR vc.NomeCompleto LIKE '%' + @nomeColaborador + '%';
+END;
+
+
+
+/*-------TESTE COM ID-------*/
+DECLARE @resultado MONEY;
+EXEC usp_RetornaBonus @idColaborador = 281, @nomeColaborador = NULL, @totalBonus = @resultado OUT;
+SELECT @resultado AS Bônus;
+
+/*-------TESTE COM NOME-------*/
+DECLARE @resultado MONEY;
+EXEC usp_RetornaBonus @idColaborador = NULL, @nomeColaborador = 'Michael G Blythe', @totalBonus = @resultado OUT;
+SELECT @resultado AS Bônus;
